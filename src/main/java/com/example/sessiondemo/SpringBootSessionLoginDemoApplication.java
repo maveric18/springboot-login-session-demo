26a3eef6a3746772a234d3c8d24d08bcf5c60eb4
Sachin Raju Patil <sarajupa@tibco.com>
2023-08-07 11:07:07 +0530

    Inital Commit

---
+++ src/main/java/com/example/sessiondemo/SpringBootSessionLoginDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sessiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSessionLoginDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSessionLoginDemoApplication.class, args);
+	}
+
+}
